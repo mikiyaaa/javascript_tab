84890499c443cdb0acfcb6591092bdd7b62e5561
mikiyaaa <katocha.m0220thankyou@gmail.com>
2021-06-04 16:18:48 +0900

    タブ機能実装

---
+++ app.js
@@ -0,0 +1,45 @@
+// 即時関数で囲う・グローバル汚染を防ぐ
+(() => {
+  const $doc = document;
+  const $tab = $doc.getElementById('js-tab');
+  const $nav = $tab.querySelectorAll('[data-nav]');
+  const $content = $tab.querySelectorAll('[data-content]');
+
+  // 初期化
+  const init = () => {
+    $content[0].style.display = 'block';
+  };
+  init();
+
+  const handleClick = (e) => {
+    // aタグのリンクを無効にする
+    e.preventDefault();
+
+    // クリックされたnavとそのdataを取得
+    const $this = e.target;
+    const targetValue = $this.dataset.nav;
+    
+    // 対象外のnav, contentを全てリセットする
+    let index = 0;
+    while(index < $nav.length) {
+      $content[index].style.display = 'none';
+      $nav[index].classList.remove('is-active');
+      index++;
+    }
+
+    // 対象のコンテンツをアクティブ化する
+     $tab.querySelectorAll('[data-content="' + targetValue + '"]')[0].style.display = 'block';
+     $nav[targetValue].classList.add('is-active');
+     
+  };
+
+
+  // 全nav要素に対して関数を呼び出す・発火
+  let index = 0;
+  while(index < $nav.length) {
+    $nav[index].addEventListener('click', (e) => handleClick(e));
+    index++;
+  }
+  
+
+})();
